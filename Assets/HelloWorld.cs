9785599465460f9ca06ff15cc156ce8f3dacf874
Skywang1 <46367918+Skywang1@users.noreply.github.com>
2020-08-06 16:08:51 +1000

    Test

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    
+
+    public GameObject ourGameObject;
+
+
+    void Update()
+    {
+        Vector3 gameObjectPosition = ourGameObject.transform.position;
+
+        gameObjectPosition.z = gameObjectPosition.z + (5f * Time.deltaTime);
+
+        ourGameObject.transform.position = gameObjectPosition;
+
+
+    }
+}
