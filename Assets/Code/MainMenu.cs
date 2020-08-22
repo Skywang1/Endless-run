7e5d565ac842f6d749a1ba8d64728cffd070d9b9
HiryuJupiter <xooase@gmail.com>
2020-08-22 15:44:23 +1000

    Starting to work on Main Menu
    Added new scenes: scene_MainMenu, scene_running
    Created canvas for menu

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Dropdown Dropdown_Qualities;
+    public Dropdown Dropdown_Resolutions;
+    public Toggle Toggle_Fullscreen;
+
+
+    const int SceneIndex_Running = 1;
+    const string Key_Fullscreen = "Fullscreen";
+    const string Key_Width = "Width";
+    const string Key_Height = "Height";
+
+
+    Resolution[] allResolutions;
+    bool isFullscreen;
+
+    #region MonoBehavior
+    void Start()
+    {
+        Load();
+
+        //Populate quality
+        Dropdown_Resolutions.ClearOptions();
+        allResolutions = Screen.resolutions;
+        List<string> options = new List<string>();
+        int resolutionIndex = 0;
+
+        // Print the resolutions
+        for (int i = 0; i < allResolutions.Length; i++)
+        {
+            options.Add(allResolutions[i].width + "x" + allResolutions[i].height);
+
+            if (Screen.width == allResolutions[i].width && Screen.height == allResolutions[i].height)
+            {
+                resolutionIndex = i;
+            }
+        }
+
+        Dropdown_Resolutions.AddOptions(options);
+        Dropdown_Resolutions.value = resolutionIndex;
+        Dropdown_Resolutions.RefreshShownValue();
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Public
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneIndex_Running);
+    }
+
+    public void SetResolution (int index)
+    {
+
+    }
+
+    public void SetQuality (int index)
+    {
+
+
+    }
+
+    public void SaveSettings ()
+    {
+        Save();
+    }
+    #endregion
+
+    #region Load/Save
+    void Load()
+    {
+        isFullscreen = PlayerPrefs.GetInt(Key_Fullscreen, Screen.fullScreen ? 1 : 0)  == 1 ? true : false;
+        Toggle_Fullscreen.isOn = isFullscreen;
+    }
+
+    void Save()
+    {
+
+    }
+    #endregion
+}
---
+++ Assets/Code/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Dropdown Qualities;
+    public Dropdown Resolutions;
+    public Toggle FullScreen;
+
+
+    const int SceneIndex_Running = 1;
+
+
+    #region MonoBehavior
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Public
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneIndex_Running);
+    }
+    #endregion
+
+    #region Load/Save
+    void Load ()
+    { 
+
+    }
+
+    void Save ()
+    {
+
+    }
+    #endregion
+}
