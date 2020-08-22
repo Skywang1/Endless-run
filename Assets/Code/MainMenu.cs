422ff25c7d1ac2382104e89c2a1e47801bffc08d
HiryuJupiter <xooase@gmail.com>
2020-08-23 09:52:29 +1000

    Main menu logic nearly done

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 //Make sure the game's quality settings only have 3 options, as we won't be changing them here.
 public class MainMenu : MonoBehaviour
@@ -14,72 +15,94 @@ public class MainMenu : MonoBehaviour
 
     const int SceneIndex_Running = 1;
     const string Key_Fullscreen = "Fullscreen";
-    const string Key_Width = "Width";
-    const string Key_Height = "Height";
     const string Key_Quality = "Quality";
     const string Key_Resolution = "Resolution";
 
     //Cache Options menu values as fields so we can save them in player prefs later.
     Resolution[] allResolutions;
-    int isFullscreen;
+    bool isFullscreen;
     int resolutionIndex;
     int qualityIndex;
 
     #region MonoBehavior
     void Start()
     {
-
+        Load_OptionsSettings();
     }
 
     void OnGUI ()
     {
         GUI.Label(new Rect(20, 20, 2000, 20), "Quality: " + QualitySettings.GetQualityLevel());
+        GUI.Label(new Rect(20, 40, 2000, 20), "isFullscreen: " + isFullscreen);
     }
     #endregion
 
-    #region Public
+    #region Public - Main menu
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneIndex_Running);
     }
 
-    public void SetResolution (int index)
+    public void Quit ()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+        Application.Quit();
+    }
+    #endregion
+
+    #region Public - Options Menu
+
+    public void SetResolution(int index)
     {
         resolutionIndex = index;
+        if (allResolutions.Length == 0)
+        {
+            PopulateResolutionDropdownBox();
+        }
+
+        Screen.SetResolution(allResolutions[index].width, allResolutions[index].height, Screen.fullScreen);
     }
 
-    public void SetQuality (int index)
+    public void SetQuality(int index)
     {
         qualityIndex = index;
         QualitySettings.SetQualityLevel(index);
     }
 
-    public void SetFullScreen (bool b)
+    public void SetFullScreen(bool b)
     {
-        isFullscreen = b == true ? 1 : 0;
+        isFullscreen = b;
         Screen.fullScreen = b;
     }
     #endregion
 
     #region Load/Save
-    public void Load()
+    public void Load_OptionsSettings()
     {
         //Load isFullscreen & set value in UI
-        isFullscreen = PlayerPrefs.GetInt(Key_Fullscreen, Screen.fullScreen ? 1 : 0);
-        Toggle_Fullscreen.isOn = isFullscreen == 1 ? true : false;
+        isFullscreen = PlayerPrefs.GetInt(Key_Fullscreen, Screen.fullScreen ? 1 : 0) == 1 ? true : false;
+        Debug.Log("isFullscreen " + isFullscreen);
+        Toggle_Fullscreen.isOn = isFullscreen;
+        SetFullScreen(isFullscreen);
 
         //Load quality & set value in UI
-        qualityIndex = PlayerPrefs.GetInt(Key_Quality, 0);        
+        qualityIndex = PlayerPrefs.GetInt(Key_Quality, 0);
+        Dropdown_Qualities.value = qualityIndex;
+        SetQuality(qualityIndex);
 
-        //Load resolution & set value in UI
+        //Load resolution & set value in UI (Do not set resolution)
         resolutionIndex = PlayerPrefs.GetInt(Key_Resolution, 0);
         PopulateResolutionDropdownBox();
     }
 
-    public void Save()
+    public void Save_OptionsSettings()
     {
         //Save isFullscreen
-        PlayerPrefs.SetInt(Key_Fullscreen, isFullscreen);
+        PlayerPrefs.SetInt(Key_Fullscreen, isFullscreen == true ? 1 : 0);
+        Debug.Log("isFullscreen " + isFullscreen);
+        Debug.Log("PlayerPrefs.GetInt(Key_Fullscreen, Screen.fullScreen " + PlayerPrefs.GetInt(Key_Fullscreen, -1));
 
         //Save quality
         PlayerPrefs.SetInt(Key_Quality, qualityIndex);
