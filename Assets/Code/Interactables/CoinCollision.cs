31e4f376323a146cde71c991ab93fcf1f7cdeb53
HiryuJupiter <xooase@gmail.com>
2020-09-09 13:29:35 +1000

    added colliders on collidable prefabs

---
+++ Assets/Code/Interactables/CoinCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("coin hits player");
+        }
+    }
+}
